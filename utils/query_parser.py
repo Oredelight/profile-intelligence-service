89ce3d04feaf17f2e945504a34c7cee7fda96a33
Oredelight <oredelight@gmail.com>
2026-04-23 09:53:28 +0100

    feat(query_parser): add natural language query parsing

--- utils/query_parser.py
+++ utils/query_parser.py
@@ -11,41 +11,39 @@ def parse_query(q: str):
     filters = {}
 
     # Gender
-    if "female" in q:
-        filters["gender"] = "female"
-    elif "male" in q:
-        filters["gender"] = "male"
+    if "male" in q and "female" not in q:
+        pass
 
     # Age group
     if "child" in q:
         filters["age_group"] = "child"
-    elif "teenager" in q:
+    elif "teen" in q:
         filters["age_group"] = "teenager"
     elif "adult" in q:
         filters["age_group"] = "adult"
     elif "senior" in q:
         filters["age_group"] = "senior"
 
-    # Young
+    # Young (special rule)
     if "young" in q:
         filters["min_age"] = 16
         filters["max_age"] = 24
 
-    # Above / below
+    # Above age
+    import re
     match = re.search(r"above (\d+)", q)
     if match:
         filters["min_age"] = int(match.group(1))
 
-    match = re.search(r"below (\d+)", q)
-    if match:
-        filters["max_age"] = int(match.group(1))
-
     # Country
-    for name, code in COUNTRY_KEYWORDS.items():
+    countries = {
+        "nigeria": "NG",
+        "kenya": "KE",
+        "angola": "AO"
+    }
+
+    for name, code in countries.items():
         if name in q:
             filters["country_id"] = code
 
-    if not filters:
-        return None
-
-    return filters
+    return filters if filters else None
